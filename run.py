04b72a4edaa20f245f0b3b00ae61e3f2a4f02fd8
mistersouza <>
2023-10-26 21:48:31 +0000

    def get_sales_figures to get user sales input

--- run.py
+++ run.py
@@ -12,8 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_figures():
+    '''
+    Get sales figures input
+    '''
+    print('Please type in last sales figures')
+    print('Figures should be six numbers, separated by commas')
+    print('Example: 10,20,30,40,50,60\n')
 
-data = sales.get_all_values()
+    data_str = input('Enter figures here: ')
+    print(f'The figures provided are {data_str}')
 
-print(data)
+get_sales_figures()
