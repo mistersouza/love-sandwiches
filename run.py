b905b721c52240d6fba65e7590aaf1e178057ec4
mistersouza <>
2023-10-28 22:53:51 +0000

    Expand get_sales_figures to run till inputted values are valid

--- run.py
+++ run.py
@@ -17,14 +17,20 @@ def get_sales_figures():
     '''
     Get sales figures input
     '''
-    print('Please type in last sales figures')
-    print('Figures should be six numbers, separated by commas')
-    print('Example: 10,20,30,40,50,60\n')
+    while True:
+        print('Please type in last sales figures')
+        print('Figures should be six numbers, separated by commas')
+        print('Example: 10,20,30,40,50,60\n')
 
-    figures_str = input('Enter figures here: ')
+        figures_str = input('Enter figures here: ')
 
-    sales_figures = figures_str.split(',')
-    validate_figures(sales_figures)
+        sales_figures = figures_str.split(',')
+
+        if validate_figures(sales_figures):
+            print('Valid values')
+            break
+
+    return sales_figures
 
 
 def validate_figures(values):
@@ -32,8 +38,7 @@ def validate_figures(values):
     Try converting all string values into integers.
     Raises ValueError is strings cannot be converted into integers,
     or there aren't exactly 6 values
-    '''
-    print(values)
+    '''      
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -42,6 +47,8 @@ def validate_figures(values):
             )
     except ValueError as error:
         print(f'Invalid data: {error}, please try again.\n')
+        return False     
+    return True
 
 
 get_sales_figures()
